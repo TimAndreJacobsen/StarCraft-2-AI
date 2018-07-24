ab04bf212ab70c6f2e00b5f3a4aeb17b5b51a72d
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-07-24 15:54:52 +0200

    import * from sc2.constants
    Planning to expand into more units and buildings and this list is already getting quite long.

--- protoss/protossAI.py
+++ protoss/protossAI.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, STALKER, GATEWAY, CYBERNETICSCORE
+from sc2.constants import *
 import random
 
 
